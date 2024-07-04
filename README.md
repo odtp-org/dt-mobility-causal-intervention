c6ec852bf975030234e5baa2f0e55aca00bcef30
caviri <45425937+caviri@users.noreply.github.com>
2024-07-04 12:20:05 +0200

    feat(functional execution): Compatible with odtp v0.4.0

--- README.md
+++ README.md
@@ -19,4 +19,15 @@ graph LR
 
 ## Tutorial
 
-TBD
+1. Clone this repository
+2. Edit dt-example.sh with the ODTP user email
+3. Configure the parameters. 
+4. Rename `001.secrets.dist` and `002.secrets.dist` to `001.secrets` and `002.secrets`. Add your credentials. 
+5. Run the bash script: sh dt-mobility-causal-intervention.sh
+
+
+How to remove the execution? This command will delete any mongoDB entry and the content of the execution folder, allowing for a fresh execution.
+
+```bash
+odtp execution delete --execution-name execution --project-path $(pwd)/dt-mobility-causal-intervention/execution
+```
--- dt-mobility-causal-intervention.sh
+++ dt-mobility-causal-intervention.sh
@@ -1,40 +1,66 @@
 #!/bin/bash
-ODTP_USER_EMAIL=
+ODTP_USER_EMAIL=maxm@gmail.com
 DT_PATH=$(pwd)
 
 DIGITAL_TWIN_NAME=dt-mobility-causal-intervention
-EXECUTION_NAME=dt-mobility-causal-intervention-execution-1
+EXECUTION_NAME=execution
 
-# Pulling all the components and versions
+rm  ${DT_PATH}/${DIGITAL_TWIN_NAME}/${EXECUTION_NAME}/.gitkeep
+
+# # Pulling all the components and versions
+# odtp new odtp-component-entry \
+# --name odtp-sql-dataloader \
+# --component-version v0.1.0 \
+# --repository https://github.com/odtp-org/odtp-sql-dataloader
+
+# odtp new odtp-component-entry \
+# --name odtp-postgis-dataloader \
+# --component-version v0.1.1 \
+# --repository https://github.com/odtp-org/odtp-postgis-dataloader
+
+# odtp new odtp-component-entry \
+# --name odtp-mobility-simulation \
+# --component-version v0.1.0 \
+# --repository https://github.com/odtp-org/odtp-mobility-simulation
+
+# odtp new odtp-component-entry \
+# --name odtp-mobility-metrics \
+# --component-version v0.0.1 \
+# --repository https://github.com/odtp-org/odtp-mobility-metrics
+
+# odtp new odtp-component-entry \
+# --name odtp-next-location-prediction \
+# --component-version v0.0.1 \
+# --repository https://github.com/odtp-org/odtp-next-location-prediction
+
 odtp new odtp-component-entry \
---name odtp-sql-dataloader \
---component-version v0.0.1 \
---repository https://github.com/odtp-org/odtp-sql-dataloader
+--name odtp-pygwalker \
+--component-version 0.1.3 \
+--repository https://github.com/odtp-org/odtp-pygwalker
 
-odtp new odtp-component-entry \
---name odtp-postgis-dataloader \
---component-version v0.0.1 \
---repository https://github.com/odtp-org/odtp-postgis-dataloader
+# # Creating new digital twin
+# odtp new digital-twin-entry \
+# --user-email ${ODTP_USER_EMAIL}  \
+# --name ${DIGITAL_TWIN_NAME}
 
-odtp new odtp-component-entry \
---name odtp-mobility-simulation \
---component-version v0.0.1 \
---repository https://github.com/odtp-org/odtp-mobility-simulation
+# # Creating new execution
+odtp new execution-entry \
+--name ${EXECUTION_NAME} \
+--digital-twin-name ${DIGITAL_TWIN_NAME} \
+--component-tags odtp-sql-dataloader:v0.1.0,odtp-postgis-dataloader:v0.1.1,odtp-mobility-simulation:v0.1.0,odtp-pygwalker:v0.1.3 \
+--parameter-files ${DT_PATH}/${DIGITAL_TWIN_NAME}/001.parameters,${DT_PATH}/${DIGITAL_TWIN_NAME}/002.parameters,${DT_PATH}/${DIGITAL_TWIN_NAME}/003.parameters, \
+--ports ,,,8555:8501
 
-odtp new odtp-component-entry \
---name odtp-mobility-metrics \
---component-version v0.0.1 \
---repository https://github.com/odtp-org/odtp-mobility-metrics
+# Preparing execution
+odtp execution prepare \
+--execution-name ${EXECUTION_NAME} \
+--project-path ${DT_PATH}/${DIGITAL_TWIN_NAME}/${EXECUTION_NAME}
 
-odtp new odtp-component-entry \
---name odtp-next-location-prediction \
---component-version v0.0.1 \
---repository https://github.com/odtp-org/odtp-next-location-prediction
+# Running execution
+odtp execution run \
+--execution-name ${EXECUTION_NAME} \
+--secrets-files ${DT_PATH}/${DIGITAL_TWIN_NAME}/001.secrets,${DT_PATH}/${DIGITAL_TWIN_NAME}/002.secrets,, \
+--project-path ${DT_PATH}/${DIGITAL_TWIN_NAME}/${EXECUTION_NAME} \
 
-# Creating new digital twin
-odtp new digital-twin-entry \
---user-email ${ODTP_USER_EMAIL}  \
---name ${DIGITAL_TWIN_NAME}
 
-# Creating new execution
-# TBD
+#odtp execution delete --execution-name execution --project-path ${DT_PATH}/${DIGITAL_TWIN_NAME}/${EXECUTION_NAME}
