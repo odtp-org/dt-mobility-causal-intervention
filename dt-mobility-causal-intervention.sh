910353f41139c8a28cbf7606044bed4f5f7509e9
Carlos Vivar Rios <carlosvivarrios@gmail.com>
2024-07-18 13:51:46 +0200

    Updated pipeline with newer version

--- dt-mobility-causal-intervention.sh
+++ dt-mobility-causal-intervention.sh
@@ -2,8 +2,8 @@
 ODTP_USER_EMAIL=maxm@gmail.com
 DT_PATH=$(pwd)
 
-DIGITAL_TWIN_NAME=dt-mobility-causal-intervention
-EXECUTION_NAME=execution
+DIGITAL_TWIN_NAME=dt-mobility-causal-intervention-full
+EXECUTION_NAME=execution-1
 
 rm  ${DT_PATH}/${DIGITAL_TWIN_NAME}/${EXECUTION_NAME}/.gitkeep
 
@@ -25,12 +25,12 @@ odtp new odtp-component-entry \
 
 odtp new odtp-component-entry \
 --name odtp-mobility-metrics \
---component-version v0.0.1 \
+--component-version v0.0.2 \
 --repository https://github.com/odtp-org/odtp-mobility-metrics
 
 odtp new odtp-component-entry \
 --name odtp-next-location-prediction \
---component-version v0.0.1 \
+--component-version v0.0.2 \
 --repository https://github.com/odtp-org/odtp-next-location-prediction
 
 odtp new odtp-component-entry \
@@ -47,7 +47,7 @@ odtp new digital-twin-entry \
 odtp new execution-entry \
 --name ${EXECUTION_NAME} \
 --digital-twin-name ${DIGITAL_TWIN_NAME} \
---component-tags odtp-sql-dataloader:v0.1.0,odtp-postgis-dataloader:v0.1.1,odtp-mobility-simulation:v0.1.0,odtp-mobility-metrics:v0.0.1,odtp-next-location-prediction:v0.0.1,odtp-pygwalker:v0.1.3 \
+--component-tags odtp-sql-dataloader:v0.1.0,odtp-postgis-dataloader:v0.1.1,odtp-mobility-simulation:v0.1.0,odtp-mobility-metrics:v0.0.2,odtp-next-location-prediction:v0.0.2,odtp-pygwalker:v0.1.3 \
 --parameter-files ${DT_PATH}/${DIGITAL_TWIN_NAME}/001.parameters,${DT_PATH}/${DIGITAL_TWIN_NAME}/002.parameters,${DT_PATH}/${DIGITAL_TWIN_NAME}/003.parameters,${DT_PATH}/${DIGITAL_TWIN_NAME}/004.parameters,${DT_PATH}/${DIGITAL_TWIN_NAME}/005.parameters, \
 --ports ,,,,,8555:8501
 
